ba6a8a3e44675a72415b45fe1d3d67b936d72fdf
tjoselemonade <temitayo@lemonade.finance>
2024-01-26 01:47:07 +0100

    first commit

--- scripts/start-sdk.sh
+++ scripts/start-sdk.sh
@@ -4,8 +4,8 @@
 set -e #stop immediately if any error happens
 
 # export $JAVA_HOME=/usr/lib/jvm/temurin-11-jdk-amd64
-yes | /usr/lib/jvm/temurin-11-jdk-amd64/bin/java -jar /opt/androidsdk/tools/bin/sdkmanager "platform-tools" "platforms;android-28" "emulator"
-yes | /usr/lib/jvm/temurin-11-jdk-amd64/bin/java -jar /opt/androidsdk/tools/bin/sdkmanager "system-images;android-28;google_apis;x86_64"
+yes |  /opt/androidsdk/tools/bin/sdkmanager "platform-tools" "platforms;android-28" "emulator"
+yes | /opt/androidsdk/tools/bin/sdkmanager "system-images;android-28;google_apis;x86_64"
 
 emulator -version
 
