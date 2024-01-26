579987eaeece43fc5b0a2aa504bcd29a7776ce12
tjoselemonade <temitayo@lemonade.finance>
2024-01-26 14:05:00 +0100

    first commit

--- scripts/start-sdk.sh
+++ scripts/start-sdk.sh
@@ -5,12 +5,14 @@ set -e #stop immediately if any error happens
 
 # export $JAVA_HOME=/usr/lib/jvm/temurin-11-jdk-amd64
 
-ln -s /opt/androidsdk/tools/emulator /usr/local/bin/emulator
+# ln -s /opt/androidsdk/tools/emulator /usr/local/bin/emulator
 yes |  /opt/androidsdk/tools/bin/sdkmanager "platform-tools" "platforms;android-28" "emulator"
 yes | /opt/androidsdk/tools/bin/sdkmanager "system-images;android-28;google_apis;x86_64"
 
 
 #  /opt/androidsdk/tools/emulator
-emulator -version
+/opt/androidsdk/tools/emulator -version
 
 echo "INSTALL ANDROID SDK DONE!"
+
+/path/to/android-sdk/tools
